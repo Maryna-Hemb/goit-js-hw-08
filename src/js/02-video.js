44a7a89ec8713f2e3bc705d3262a5acac0c54329
Maryna-Hemb <marinagembarskaa@gmail.com>
2022-12-23 12:22:37 +0200

    feat: add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
+const KEY_LOCAL = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
     
@@ -9,12 +9,12 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(onPlayerCurrentTime, 1000));
 function onPlayerCurrentTime(currentTime) {
     const currentSecond = currentTime.seconds;
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(currentSecond));
+    localStorage.setItem(KEY_LOCAL, JSON.stringify(currentSecond));
 }
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_LOCAL)))
     .then(function (seconds) {
-    seconds = JSON.parse(localStorage.getItem("videoplayer-current-time"))
+    seconds = JSON.parse(localStorage.getItem(KEY_LOCAL))
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector(".feedback-form");
+const inputEl = formEl.querySelector("input");
+const messageEl = formEl.querySelector("textarea");
+
+const KEY_LOCAL = "feedback-form-state";
+formEl.addEventListener("submit", onFormReset)
+inputEl.addEventListener("input", throttle(onInputFillField, 500));
+messageEl.addEventListener("input", throttle(onInputFillField, 500));
+checkInput()
+
+
+
+
+function onInputFillField() {
+    localStorage.setItem(KEY_LOCAL, JSON.stringify({name: inputEl.value, message: messageEl.value}))
+}
+
+
+
+function checkInput() {
+    const savedInputValues = JSON.parse(localStorage.getItem(KEY_LOCAL));
+    if (savedInputValues) {
+        inputEl.value = savedInputValues.name;
+        messageEl.value = savedInputValues.message
+    }
+} 
+
+function onFormReset(e) {
+    e.preventDefault();
+     console.log(JSON.parse(localStorage.getItem(KEY_LOCAL))); 
+     e.currentTarget.reset();
+    localStorage.removeItem(KEY_LOCAL);
+   
+}
+
+
 
